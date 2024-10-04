d418a2e50ae18763487fe91aeadd48eed61fb2ea
Ofek Asolin <the-lifelong-student@protonmail.com>
2024-10-04 14:57:00 +0300

    Added components layer to tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,14 @@
 import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 
+/** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.tsx"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
-      },
-    },
-  },
-  plugins: [],
+	content: [ "./src/**/*.tsx" ],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: [ "var(--font-geist-sans)", ...fontFamily.sans ],
+			},
+		},
+	},
 } satisfies Config;
