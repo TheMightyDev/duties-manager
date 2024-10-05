d20860a54dd5d339cff042c862d761c8246f8a09
Ofek Asolin <the-lifelong-student@protonmail.com>
2024-10-05 21:41:25 +0300

    Set fixed dates to seed duties

--- prisma/seedData/seedDuties.ts
+++ prisma/seedData/seedDuties.ts
@@ -1,5 +1,4 @@
 import { type Duty, DutyKind, UserRole } from "@prisma/client";
-import { addDays, subDays } from "date-fns";
 import { seedOrganizations } from "prisma/seedData/seedOrganizations";
 
 export const seedDuties = [
@@ -7,8 +6,8 @@ export const seedDuties = [
 		id: "duty1",
 		organizationId: seedOrganizations[0].id,
 		kind: DutyKind.GUARDING,
-		startDate: subDays(new Date(), 11),
-		endDate: subDays(new Date(), 10),
+		startDate: new Date(Date.UTC(2024, 8, 24, 10, 0)),
+		endDate: new Date(Date.UTC(2024, 8, 27, 10, 0)),
 		role: UserRole.SQUAD,
 		quantity: 3,
 		score: 1,
@@ -18,8 +17,8 @@ export const seedDuties = [
 		id: "duty2",
 		organizationId: seedOrganizations[0].id,
 		kind: DutyKind.GUARDING,
-		startDate: subDays(new Date(), 11),
-		endDate: subDays(new Date(), 10),
+		startDate: new Date(Date.UTC(2024, 8, 28, 10, 0)),
+		endDate: new Date(Date.UTC(2024, 8, 29, 10, 0)),
 		role: UserRole.COMMANDER,
 		quantity: 1,
 		score: 1,
@@ -29,8 +28,8 @@ export const seedDuties = [
 		id: "duty3",
 		organizationId: seedOrganizations[0].id,
 		kind: DutyKind.GUARDING,
-		startDate: addDays(new Date(), 5),
-		endDate: addDays(new Date(), 6),
+		startDate: new Date(Date.UTC(2024, 9, 7, 10, 0)),
+		endDate: new Date(Date.UTC(2024, 9, 8, 10, 0)),
 		role: UserRole.SQUAD,
 		quantity: 2,
 		score: 1,
@@ -40,8 +39,8 @@ export const seedDuties = [
 		id: "duty4",
 		organizationId: seedOrganizations[0].id,
 		kind: DutyKind.GUARDING,
-		startDate: addDays(new Date(), 20),
-		endDate: addDays(new Date(), 21),
+		startDate: new Date(Date.UTC(2024, 9, 24, 10, 0)),
+		endDate: new Date(Date.UTC(2024, 9, 27, 10, 0)),
 		role: UserRole.OFFICER,
 		quantity: 1,
 		score: 1,
