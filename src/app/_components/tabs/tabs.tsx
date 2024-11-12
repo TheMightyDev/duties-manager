7b0213998305c0e70160818f742c01df9c9918a3
Ofek Asolin <the-lifelong-student@protonmail.com>
2024-11-12 15:24:30 +0200

    Created a general tabs component and used in user profile

---
+++ src/app/_components/tabs/tabs.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { cn } from "@/app/_utils/cn";
+import React, { useState } from "react";
+
+export interface TabData {
+	title: string;
+	contents: React.ReactNode;
+}
+
+interface TabsProps {
+	tabs: TabData[];
+}
+
+function areAllTabTitles(tabs: TabData[]): boolean {
+	const titles = tabs.map((tab) => tab.title);
+	
+	return ((new Set(titles)).size === titles.length);
+}
+
+/** A general component which manages viewing of one tab at a time.
+ * Handles switching between tabs and showing current tab contents on its own.
+ */
+export function Tabs({ tabs }: TabsProps) {
+	const [ selectedTabIndex, setSelectedTabIndex ] = useState(0);
+	
+	if (tabs.length == 0) {
+		return "You must provide at least one tab to use the component!";
+	}
+	
+	if (!areAllTabTitles(tabs)) {
+		return "All tab IDs must be unique";
+	}
+
+	return (
+		<>
+			<div className="flex h-8 w-full text-center leading-8">
+				{
+					tabs.map((tab, tabIndex) => (
+						<div
+							key={tab.title}
+							className={cn(
+								"rounded-full flex-1",
+								tabIndex === selectedTabIndex ? "bg-blue-500 text-white font-bold" : "cursor-pointer hover:bg-slate-300"
+							)}
+							onClick={() => {
+								setSelectedTabIndex(tabIndex);
+							}}
+						>
+							{tab.title}
+						</div>
+					))
+				}
+			</div>
+			{
+				tabs[selectedTabIndex]?.contents
+			}
+		</>
+	);
+};
--- src/app/user-dashboard/profile/[userId]/[role]/components/user-profile.tsx
+++ src/app/user-dashboard/profile/[userId]/[role]/components/user-profile.tsx
@@ -1,4 +1,5 @@
 import { Accordion } from "@/app/_components/accordion/accordion";
+import { Tabs } from "@/app/_components/tabs/tabs";
 import { DutyAssignments } from "@/app/user-dashboard/profile/[userId]/[role]/components/duty-assignments";
 import { ProfileInfoBoxes } from "@/app/user-dashboard/profile/[userId]/[role]/components/profile-info-boxes";
 import { ProfileRoleSelector } from "@/app/user-dashboard/profile/[userId]/[role]/components/profile-role-selector";
@@ -12,15 +13,32 @@ export function UserProfile(props: UserProfileProps) {
 				roleRecords={props.roleRecords}
 				selectedRole={props.userJustice.role}
 			/>
-			<div className="flex flex-col gap-2 lg:flex-row">
+			<div className="m-auto flex max-w-96 flex-col gap-2 sm:max-w-none sm:flex-row">
 				<ProfileInfoBoxes {...props} />
-				<div className="w-full p-2">
+				<div className="block w-full p-2 sm:hidden">
 					<Accordion
 						title="היי"
 						isOpenByDefault={false}
 					>
 						<DutyAssignments {...props} />
 					</Accordion>
+				</div>
+				<div className="hidden w-full sm:block">
+					<Tabs tabs={[
+						{
+							title: "תורנויות",
+							contents: <DutyAssignments {...props} />,
+						},
+						{
+							title: "תפקידים",
+							contents: <p>yooo22</p>,
+						},
+						{
+							title: "פטורים",
+							contents: <p>yooo33</p>,
+						},
+					]}
+					/>
 				</div>
 			</div>
 		</>
