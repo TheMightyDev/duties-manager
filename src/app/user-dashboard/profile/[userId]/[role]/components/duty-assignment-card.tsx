912bba596cd36dd2c9d786212e2e1a8ee309a262
Ofek Asolin <the-lifelong-student@protonmail.com>
2024-11-23 21:08:43 +0200

    Private duties are marked with lock icon

--- src/app/user-dashboard/profile/[userId]/[role]/components/duty-assignment-card.tsx
+++ src/app/user-dashboard/profile/[userId]/[role]/components/duty-assignment-card.tsx
@@ -1,3 +1,4 @@
+import { LockSvgIcon } from "@/app/_components/svg-icons/ui/lock-svg-icon";
 import { OfficerSvgIcon } from "@/app/_components/svg-icons/user-roles/officer-svg-repo";
 import { type Duty } from "@prisma/client";
 import { format } from "date-fns";
@@ -8,12 +9,15 @@ interface DutyAssignmentCardProps {
 
 export function DutyAssignmentCard({ duty }: DutyAssignmentCardProps) {
 	return (
-		<div className="rounded-xl bg-blue-500 text-white">
+		<div className="relative rounded-xl bg-blue-500 text-white">
 			<div>
 				<OfficerSvgIcon className="size-12 fill-white"/>
 				<span className="font-bold">{duty.kind}</span>
 			</div>
+			
 			<p>{format(duty.startDate, "yyyy-MM-dd")}</p>
+			
+			{duty.isPrivate && <LockSvgIcon className="absolute end-1 top-1 size-8 stroke-white"/>}
 		</div>
 	);
 };
