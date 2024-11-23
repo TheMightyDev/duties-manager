3dee6b96fa8c00bb8ac699f5663c21b1ba296e0c
Ofek Asolin <the-lifelong-student@protonmail.com>
2024-11-23 15:24:35 +0200

    Added a retire period to profile

--- src/app/user-dashboard/profile/[userId]/[role]/profile-data-components/period-record.tsx
+++ src/app/user-dashboard/profile/[userId]/[role]/profile-data-components/period-record.tsx
@@ -2,6 +2,7 @@ import { PathLocationSvgIcon } from "@/app/_components/svg-icons/path-location-s
 import { CommanderSvgIcon } from "@/app/_components/svg-icons/user-roles/commander-svg-icon";
 import { ExemptSvgIcon } from "@/app/_components/svg-icons/user-roles/exempt-svg-icon";
 import { OfficerSvgIcon } from "@/app/_components/svg-icons/user-roles/officer-svg-repo";
+import { RetireSvgIcon } from "@/app/_components/svg-icons/user-roles/retire-svg-icon";
 import { SquadSvgIcon } from "@/app/_components/svg-icons/user-roles/squad-svg-icon";
 import { cn } from "@/app/_utils/cn";
 import { formatDate } from "@/app/_utils/date-format-utils";
@@ -11,6 +12,7 @@ import clsx from "clsx";
 interface PeriodRecordProps {
 	period: Period;
 	isCurrentPeriod: boolean;
+	isLastPeriod?: boolean;
 }
 
 const roleBg = {
@@ -24,7 +26,7 @@ const roleBorder = {
 	[UserRole.SQUAD]: "border-role-squad text-role-squad",
 	[UserRole.OFFICER]: "border-role-officer text-role-officer",
 	[UserRole.COMMANDER]: "border-role-commander text-role-commander",
-	[UserRole.EXEMPT]: "border-role-exempt fill-role-exempt",
+	[UserRole.EXEMPT]: "border-role-exempt text-role-exempt",
 } as const satisfies Record<UserRole, string>;
 
 const roleIcons = {
@@ -37,19 +39,26 @@ const roleIcons = {
 export function PeriodRecord({
 	period,
 	isCurrentPeriod,
+	isLastPeriod = false,
 }: PeriodRecordProps) {
 	const obj = {
-		elem: period.status === PeriodStatus.FULFILLS_ROLE ? roleIcons[period.role] :
-			PathLocationSvgIcon,
+		elem: isLastPeriod ?
+			RetireSvgIcon :
+			period.status === PeriodStatus.FULFILLS_ROLE ? roleIcons[period.role] :
+				PathLocationSvgIcon,
 	};
 	
 	const currentDate = new Date();
 	
 	return (
 		<>
-			<li className={clsx("mb-7", isCurrentPeriod ? "ms-7" : "ms-6")}>
+			<li className={clsx(
+				!isLastPeriod && "mb-7",
+				isCurrentPeriod ? "ms-7" : "ms-6"
+			)}
+			>
 				<span className={cn(
-					" absolute -start-4 flex size-8 items-center justify-center rounded-full ring-8 ring-slate-300 dark:bg-blue-900 dark:ring-gray-900",
+					" absolute -start-4 mt-2 flex size-8 items-center justify-center rounded-full  ring-slate-300 dark:bg-blue-900 dark:ring-gray-900 ring-8",
 					isCurrentPeriod ? "size-10 -start-5 " : "size-8 -start-4",
 					currentDate > period.startDate ? roleBg[period.role] + " text-white" : roleBorder[period.role] + " p-0.5  border-2 bg-slate-300"
 				)}
@@ -62,7 +71,11 @@ export function PeriodRecord({
 						(נוכחי)
 					</span>}
 				</h3>
-				<time className="mb-2 block text-sm font-normal leading-none text-slate-600 dark:text-slate-500">
+				<time className={clsx(
+					!isLastPeriod && "mb-2",
+					"block text-sm font-normal leading-none text-slate-600 dark:text-slate-500"
+				)}
+				>
 					{formatDate(period.startDate)}
 				</time>
 			</li>
--- src/app/user-dashboard/profile/[userId]/[role]/profile-data-components/periods-container.tsx
+++ src/app/user-dashboard/profile/[userId]/[role]/profile-data-components/periods-container.tsx
@@ -1,13 +1,30 @@
 import { PeriodRecord } from "@/app/user-dashboard/profile/[userId]/[role]/profile-data-components/period-record";
-import { type Period } from "@prisma/client";
+import { PeriodStatus, UserRole, type Period } from "@prisma/client";
 
 interface PeriodsContainerProps {
 	periods: Period[];
 }
 
 export function PeriodsContainer({ periods }: PeriodsContainerProps) {
+	if (periods.length === 0) {
+		return <>No periods found for user</>;
+	}
+	
 	const currentDate = new Date();
 	const currentPeriod: Period | undefined = periods.find((period) => currentDate > period.startDate && currentDate < period.endDate);
+	
+	// It cannot be `undefined` as there must be at least one period
+	const retireDate = periods.at(-1)!.endDate;
+	const retirePeriod: Period = {
+		id: "retire",
+		// It cannot be `undefined` as there must be at least one period
+		userId: periods[0]!.userId,
+		description: "שחרור",
+		role: UserRole.EXEMPT,
+		startDate: retireDate,
+		endDate: retireDate,
+		status: PeriodStatus.TEMPORARILY_ABSENT,
+	};
 	
 	return (
 		<div className="flex flex-col gap-2 py-3 ps-6">
@@ -16,12 +33,18 @@ export function PeriodsContainer({ periods }: PeriodsContainerProps) {
 				{
 					periods.map((period) => (
 						<PeriodRecord
-							key={period.id}
 							period={period}
 							isCurrentPeriod={currentPeriod === period}
+							key={period.id}
 						/>
 					))
 				}
+				<PeriodRecord
+					period={retirePeriod}
+					isCurrentPeriod={new Date() > retirePeriod.startDate}
+					isLastPeriod={true}
+					key={retirePeriod.id}
+				/>
 			</ol>
 			{/* {
 				periods.map((period) => (
