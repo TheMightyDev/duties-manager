24cb8fc763df8ba967dd6d5b1c4b88b42ff49f57
Ofek Asolin <the-lifelong-student@protonmail.com>
2024-11-23 14:55:38 +0200

    The my profile link in the side bar now sends you to my profile

--- src/app/_components/side-nav/nav-links.tsx
+++ src/app/_components/side-nav/nav-links.tsx
@@ -3,37 +3,54 @@
 import clsx from "clsx";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-// Map of links to display in the side navigation.
-// Depending on the size of the application, this would be stored in a database.
-const links = [
-	{
-		name: "לוח שנה",
-		href: "/user-dashboard/calendar",
-	},
-	{
-		name: "טבלת הצדק",
-		href: "/user-dashboard/justice",
-	},
-	{
-		name: "הפרופיל שלי",
-		href: "/user-dashboard/my-profile",
-	},
-];
 
-export function NavLinks() {
+interface NavLinksProps {
+	loggedUserId: string;
+}
+export function NavLinks({ loggedUserId }: NavLinksProps) {
 	const pathname = usePathname();
+	
+	// Map of links to display in the side navigation.
+	// Depending on the size of the application, this would be stored in a database.
+	const links = [
+		{
+			id: "calendar",
+			name: "לוח שנה",
+			href: "/user-dashboard/calendar",
+		},
+		{
+			id: "justice",
+			name: "טבלת הצדק",
+			href: "/user-dashboard/justice",
+		},
+		{
+			id: "my-profile",
+			name: "הפרופיל שלי",
+			href: `/user-dashboard/profile/${loggedUserId}/LATEST`,
+		},
+	];
 	console.log("pathname", pathname);
+	
+	function isOnPage(pageId: string) {
+		if (pageId === "my-profile") {
+			return pathname.startsWith(`/user-dashboard/profile/${loggedUserId}`);
+		} else {
+			const linkHref = links.find((link) => link.id === pageId);
+
+			return linkHref?.href === pathname;
+		}
+	}
 	
 	return (
 		<>
 			{links.map((link) => {
 				return (
 					<Link
-						key={link.name}
+						key={link.id}
 						href={link.href}
 						className={clsx(
 							"flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
-							pathname === link.href && "bg-sky-100 text-blue-600"
+							isOnPage(link.id) && "bg-sky-100 text-blue-600"
 						)}
 					>
 						<p className="hidden md:block">{link.name}</p>
--- src/app/_components/side-nav/side-nav.tsx
+++ src/app/_components/side-nav/side-nav.tsx
@@ -16,7 +16,7 @@ export async function SideNav() {
 				</div>
 			</Link>
 			<div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
-				<NavLinks />
+				<NavLinks loggedUserId={session?.user.id ?? ""}/>
 				<div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
 				<form>
 					<Link
