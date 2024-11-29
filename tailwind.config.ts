ca15c10564db2815df33c5e8d3925e58db3172b4
Ofek Asolin <the-lifelong-student@protonmail.com>
2024-11-29 17:53:30 +0200

    Initialized Shadcn UI components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,25 +3,72 @@ import { fontFamily } from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: [ "./src/**/*.tsx" ],
+    darkMode: ["class"],
+    content: [ "./src/**/*.tsx" ],
 	theme: {
-		extend: {
-			fontFamily: {
-				sans: [ "var(--font-geist-sans)", ...fontFamily.sans ],
-			},
-			colors: {
-				"role-squad": "#8843F2",
-				"role-officer": "#59D5E0",
-				"role-commander": "#FAA300",
-				"role-exempt": "#F4538A",
-			},
-		},
-		keyframes: {
-			shimmer: {
-				"100%": {
-					transform: "translateX(100%)",
-				},
-			},
-		},
-	},
+    	extend: {
+    		fontFamily: {
+    			sans: [ "var(--font-geist-sans)", ...fontFamily.sans ]
+    		},
+    		colors: {
+    			'role-squad': '#8843F2',
+    			'role-officer': '#59D5E0',
+    			'role-commander': '#FAA300',
+    			'role-exempt': '#F4538A',
+    			background: 'hsl(var(--background))',
+    			foreground: 'hsl(var(--foreground))',
+    			card: {
+    				DEFAULT: 'hsl(var(--card))',
+    				foreground: 'hsl(var(--card-foreground))'
+    			},
+    			popover: {
+    				DEFAULT: 'hsl(var(--popover))',
+    				foreground: 'hsl(var(--popover-foreground))'
+    			},
+    			primary: {
+    				DEFAULT: 'hsl(var(--primary))',
+    				foreground: 'hsl(var(--primary-foreground))'
+    			},
+    			secondary: {
+    				DEFAULT: 'hsl(var(--secondary))',
+    				foreground: 'hsl(var(--secondary-foreground))'
+    			},
+    			muted: {
+    				DEFAULT: 'hsl(var(--muted))',
+    				foreground: 'hsl(var(--muted-foreground))'
+    			},
+    			accent: {
+    				DEFAULT: 'hsl(var(--accent))',
+    				foreground: 'hsl(var(--accent-foreground))'
+    			},
+    			destructive: {
+    				DEFAULT: 'hsl(var(--destructive))',
+    				foreground: 'hsl(var(--destructive-foreground))'
+    			},
+    			border: 'hsl(var(--border))',
+    			input: 'hsl(var(--input))',
+    			ring: 'hsl(var(--ring))',
+    			chart: {
+    				'1': 'hsl(var(--chart-1))',
+    				'2': 'hsl(var(--chart-2))',
+    				'3': 'hsl(var(--chart-3))',
+    				'4': 'hsl(var(--chart-4))',
+    				'5': 'hsl(var(--chart-5))'
+    			}
+    		},
+    		borderRadius: {
+    			lg: 'var(--radius)',
+    			md: 'calc(var(--radius) - 2px)',
+    			sm: 'calc(var(--radius) - 4px)'
+    		}
+    	},
+    	keyframes: {
+    		shimmer: {
+    			'100%': {
+    				transform: 'translateX(100%)'
+    			}
+    		}
+    	}
+    },
+    plugins: [require("tailwindcss-animate")]
 } satisfies Config;
