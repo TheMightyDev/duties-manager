a3f7b01a20d66c04ad5bb12609b63aad0a951e4f
Ofek Asolin <the-lifelong-student@protonmail.com>
2024-11-29 17:47:13 +0200

    Tidied layout a bit

--- src/app/user-dashboard/layout.tsx
+++ src/app/user-dashboard/layout.tsx
@@ -55,16 +55,14 @@ export default async function Layout({ children }: Readonly<{ children: React.Re
 	return (
 		<div className="flex w-full flex-col md:flex-row">
 			<SideNav />
-			<main className="flex-auto  pb-20 md:pb-0 ">
+			<main className="flex-auto pb-20 md:pb-0">
 				{children}
 			</main>
 			<div className="pointer-events-none fixed bottom-16 block h-8 w-screen bg-gradient-to-t from-white md:hidden"></div>
-			<div className="fixed bottom-0 me-4 h-16 w-screen justify-around  md:hidden">
-				<BottomNavigation
-					routeInfos={routeInfos}
-					loggedUserId={loggedUserId}
-				/>
-			</div>
+			<BottomNavigation
+				routeInfos={routeInfos}
+				loggedUserId={loggedUserId}
+			/>
 		</div>
 	);
 }
